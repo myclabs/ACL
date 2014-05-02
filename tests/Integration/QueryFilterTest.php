3129b6c554605399c6abed7af8829acd3f11d924
Matthieu Napoli <matthieu@mnapoli.fr>
2014-05-02 09:58:56 +0200

    Test for #8

--- tests/Integration/QueryFilterTest.php
+++ tests/Integration/QueryFilterTest.php
@@ -64,4 +64,38 @@ class QueryFilterTest extends AbstractIntegrationTest
         $this->assertCount(1, $articles);
         $this->assertSame($article2, current($articles));
     }
+
+    /**
+     * @link https://github.com/myclabs/ACL/issues/8
+     * @test
+     */
+    public function duplicateAuthorizationShouldNotYieldEntityTwice()
+    {
+        $article1 = new Article();
+        $this->em->persist($article1);
+        $article2 = new Article();
+        $this->em->persist($article2);
+
+        $user = new User();
+        $this->em->persist($user);
+
+        $this->em->flush();
+
+        // Add the role twice => 2 authorizations for the same resource
+        $this->acl->grant($user, new ArticleEditorRole($user, $article2));
+        $this->acl->grant($user, new ArticleEditorRole($user, $article2));
+
+        // Check that there is really 2 authorizations created
+        $authorizations = $this->em->getRepository('MyCLabs\ACL\Model\Authorization')->findAll();
+        $this->assertCount(2, $authorizations);
+
+        $qb = $this->em->createQueryBuilder();
+        $qb->select('a')->from('Tests\MyCLabs\ACL\Integration\Model\Article', 'a');
+        ACLQueryHelper::joinACL($qb, $user, Actions::VIEW);
+        $articles = $qb->getQuery()->getResult();
+
+        // Check that we get the article only once
+        $this->assertCount(1, $articles);
+        $this->assertSame($article2, current($articles));
+    }
 }
